ea4ebc154f1c32c1b029f8ae60860464b641ce9d
kennek4 <kentabanay@gmail.com>
2024-11-11 13:30:49 -0700

    Update: keymaps and formatting

--- lua/config/keymaps.lua
+++ lua/config/keymaps.lua
@@ -1,3 +1,9 @@
 -- Keymaps are automatically loaded on the VeryLazy event
 -- Default keymaps that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/keymaps.lua
--- Add any additional keymaps here
+
+-- Normal and Insert Mode
+vim.api.nvim_set_keymap("i", "jj", "<esc>", { desc = "Exists" })
+
+-- Line Manipulation
+vim.api.nvim_set_keymap("n", "<C-J>", "o<Esc>k", { desc = "Inserts a a new line under" })
+vim.api.nvim_set_keymap("n", "<C-K>", "O<Esc>j", { desc = "Inserts a a new line over" })
